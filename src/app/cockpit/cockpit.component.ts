b77bc46eb10757e0d48a8a33faf611e458780255
Lev Tsenovoy <lev@Levs-MacBook-Pro.local>
2021-01-24 22:30:41 -0500

    finished video 70

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ import {EventEmitter} from '@angular/core';
 export class CockpitComponent implements OnInit {
 
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
 
